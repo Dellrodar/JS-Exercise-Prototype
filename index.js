53a3d3eeec865998cd9b60cea05b2e57de2cc8b6
Emilio Crocco <3357168+Dellrodar@users.noreply.github.com>
2020-03-17 14:49:15 -0400

    Adding some solutions for Car prootype.

--- index.js
+++ index.js
@@ -74,8 +74,20 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons) {
+  this.tank = gallons + this.tank;
+}
+
+Car.prototype.drive = function(distance) {
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance/this.milesPerGallon);
 }
 
 /*
